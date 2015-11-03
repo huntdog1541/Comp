76425613822296934df7fa7968d96120263cfb37
David Hunt <huntdog1541@gmail.com>
2015-11-03 00:05:08 -0600

    added files and update content

--- include/harddrive.h
+++ include/harddrive.h
@@ -6,9 +6,12 @@ using namespace std;
 class HardDrive
 {
 public:
+  void formatHardDrive();
+  void readHardDrive();
 
 private:
   string hdFileName;
   bool isFormatted;
+  FILE * hd;
 
 };
--- src/harddrive.cpp
+++ src/harddrive.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 #include <string>
 #include "harddrive.h"
+
+void HardDrive::formatHardDrive()
+{
+	FILE * fin = fopen("harddrive.hdf", "w+");
+	char bin[512], var = '/0';
+	for(int i = 0; i < 512; i++)
+	{
+		bin[i] = var;
+	}
+	
+	fclose(fin);
+}
+
+void HardDrive::readHardDrive()
+{
+	
+}
